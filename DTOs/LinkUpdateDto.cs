4119822da0d95ae80013173a34577a57ebf1bd1b
oricardos <ricardo.rs95@gmail.com>
2025-11-22 20:33:17 -0300

    adicionando validacao e tratando erros

--- DTOs/LinkUpdateDto.cs
+++ DTOs/LinkUpdateDto.cs
@@ -2,10 +2,19 @@
 
 namespace LinksApi.DTOs
 {
-    public class LinkCreateDto
+    public class LinkUpdateDto
     {
+        [Required]
+        [MaxLength(120)]
         public string Name { get; set; }
+
+        [Required]
+        [Url]
+        [MaxLength(2000)]
         public string Url { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Category { get; set; }
     }
 }
